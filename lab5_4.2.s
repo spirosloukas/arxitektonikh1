d424bdfc1563f40c793301d2d5c5cf1cb4a7b20b
spirosloukas <75336283+spirosloukas@users.noreply.github.com>
2023-05-18 11:02:20 +0300

    Update lab5_4.2.s

--- lab5_4.2.s
+++ lab5_4.2.s
@@ -12,35 +12,54 @@ main:
 
 
 # start of main program
- la $a0,prompt
+la $a0,prompt
 
- li $v0,4
- syscall # display "Enter integer number :"
+li $v0,4
+syscall # display "Enter integer number :"
  
- li $v0,5
- syscall # read integer
+li $v0,5
+syscall # read integer
+move $a0,$v0
 
- move $t0,$v0
- la $a0,endl
- li $v0,4
- syscall # display end of line
+jal proc
 
-
- move $a0,$t0 
-
+la $a0,endl
+li $v0,4
+syscall # display end of line
 
 
- li $v0,10
- syscall # exit
+move $a0,$t0 
+
+
+
+li $v0,10
+syscall # exit
 # end of main program
 
 
 # start of procedure
+proc:
+    addi $sp, $sp, -8
+    sw $a0 , 0($sp)
+    sw $ra , 4($sp)
+    beq $a0, $zero, end
+    addi $a0 , $a0 ,-1
+    jal proc
 
+    end:
 
+    li $v0,1
+    lw $a0, 0($sp)
+    syscall
 
-
-
+    la $a0,endl
+    li $v0,4
+    syscall # display end of line
+    
+    addi $sp, $sp, 4
+    lw $ra , 0($sp)
+    addi $sp, $sp ,4
+    jr $ra
 
 # end of procedure
 
@@ -51,7 +70,7 @@ main:
 #################################################
 .data
 endl: .asciiz "\n"
-
+prompt: .asciiz "Enter integer number :" 
 
 #################################################
 # #
